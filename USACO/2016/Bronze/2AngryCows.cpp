b12e36f1b3139624c47b951f5421c750b78e08dc
Salty-Beggar <ivolijuguedes@gmail.com>
2026-04-22 17:02:13 -0300

    Partial solving angry cows

--- USACO/2016/Bronze/2AngryCows.cpp
+++ USACO/2016/Bronze/2AngryCows.cpp
@@ -1,3 +1,37 @@
 
 #include <iostream>
 #include <cstdio>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+long int x;
+vector<long int> bales;
+stack<pair<long int, int>> curSoonBlows, curLateBlows;
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        long int x;
+        cin >> x;
+        bales.push_back(x);
+    }
+    sort(bales.begin(), bales.end());
+
+    curSoonBlows.push({bales[0], 1});
+    curLateBlows.push({bales[0], 1});
+    int maxBlows = 1;
+    for (int i = 1; i < n; i++) {
+        int curRadius = 1;
+        while (curSoonBlows.top().first <= bales[i]-curRadius) {
+            
+        }
+        while (curLateBlows.top().first+curLateBlows.top().second <= bales[i]) {
+            
+        }
+    }
+}
