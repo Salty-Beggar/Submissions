d0632a72897073ebee709a8917b5b900943a6aed
Salty-Beggar <ivolijuguedes@gmail.com>
2026-04-30 11:00:34 -0300

    NEw race

--- USACO/2020/Bronze/3Race.cpp
+++ USACO/2020/Bronze/3Race.cpp
@@ -10,13 +10,14 @@ long int k;
 
 int main()
 {
-    
+    freopen("race.in", "r", stdin);
+    freopen("race.out", "w", stdout);
     cin >> k >> n;
     for (int i = 0; i < n; i++) {
         long int x;
         cin >> x;
         if (x*(x+1)/2 >= k) {
-            // v(v+1) = k
+            // v(v+1)/2 = k
             // v^2 + v - 2k = 0
             long int delta = ceil(sqrt(1+4*2*k));
             cout << (delta-1)/2;
@@ -26,12 +27,10 @@ int main()
             if (v*v == delta) v--;
             long int ans = 2*v-x-1;
             long int remain = k-v*v+x*(x+1)/2;
-            // cout << remain << "|";
             long int topLeaps = (remain-1)/v;
             remain -= topLeaps*v;
-            long int bottomLeaps = (remain+x-1)/x;
+            long int bottomLeaps = (remain == 1) ? 1 : 2;
             ans += topLeaps+bottomLeaps;
-            // cout << v << "|";
             cout << ans;
         }
         cout << "\n";
